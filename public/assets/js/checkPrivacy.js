cecbac5dc9977b8bc87bc16d79d40de9f27ec706
Maden51 <madenon123@gmail.com>
2022-11-16 11:14:12 +0300

    переделан футер и обновлены логотипы

--- public/assets/js/checkPrivacy.js
+++ public/assets/js/checkPrivacy.js
@@ -15,30 +15,4 @@ if ($btn != null)
             $btn.style.borderColor = '#83897D'
         }
     });
-
 }
-
-const $footerBtn = document.querySelector('#footerBtn'),
-    $footerCheckbox = document.querySelector('#footerCheckbox');
-
-$footerBtn.style.backgroundColor = '#adb6a2';
-
-$footerCheckbox.addEventListener('change', () => {
-    if ($footerCheckbox.checked) {
-        $footerBtn.disabled = false;
-        $footerBtn.style.backgroundColor = '#9AD351';
-        $footerBtn.style.borderColor = '#7E9E55'
-        $footerBtn.addEventListener('mouseover', () => {
-            $footerBtn.style.backgroundColor = '#282828';
-            $footerBtn.style.color = '#fff';
-        })
-        $footerBtn.addEventListener('mouseout', () => {
-            $footerBtn.style.backgroundColor = '#9AD351';
-            $footerBtn.style.color = '#282828';
-        })
-    } else {
-        $footerBtn.disabled = true;
-        $footerBtn.style.backgroundColor = '#adb6a2';
-        $footerBtn.style.borderColor = '#83897D'
-    }
-});
