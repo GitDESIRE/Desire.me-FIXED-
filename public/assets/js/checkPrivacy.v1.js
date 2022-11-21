71f85d58be9a2052c3259cbb9c166b376c5e6ac3
Maden51 <madenon123@gmail.com>
2022-11-21 16:33:23 +0300

    версии файлов

---
+++ public/assets/js/checkPrivacy.v1.js
@@ -0,0 +1,17 @@
+const $btn = document.querySelector('#btn'),
+    $checkbox = document.querySelector('#input');
+if ($btn != null)
+{
+    $btn.style.backgroundColor = '#adb6a2';
+
+    $checkbox.addEventListener('change', () => {
+        if ($checkbox.checked) {
+            $btn.disabled = false;
+            $btn.style.backgroundColor = '#9AD351';
+            $btn.style.borderColor = '#9AD351'
+        } else {
+            $btn.disabled = true;
+            $btn.style.backgroundColor = '#adb6a2';
+        }
+    });
+}
