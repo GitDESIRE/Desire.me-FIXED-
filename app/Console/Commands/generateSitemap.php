342e89b80b58dddb2a62ceeaf319278aa57e24a3
Gonreg <109808538+Gonreg@users.noreply.github.com>
2022-11-21 14:55:34 +0300

    siteMap

--- app/Console/Commands/generateSitemap.php
+++ app/Console/Commands/generateSitemap.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use DateTime;
 use Illuminate\Console\Command;
+use SimpleXMLElement;
 use Watson\Active\Route;
 
 class generateSitemap extends Command
@@ -31,28 +32,16 @@ class generateSitemap extends Command
     {
         $defaultdate = new DateTime('2018-01-01 10:01:01');
 
-        $site_url = env('APP_URL');
         $base =
             '<?xml version="1.0" encoding="UTF-8"?>
             <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
             </urlset>';
 
-        $xmlbase = new \SimpleXMLElement($base);
+        $xmlbase = new SimpleXMLElement($base);
 
         $routes = ['privacy.polity', 'index', 'service.web', 'service.promotion',
             'service.rate', 'service.star', 'service.arrow', 'portfolio', 'portfolio.dolfie',
-            'portfolio.nike', 'portfolio.noiseland', 'portfolio.tocha', 'about', 'new'];
-       /* $row  = $xmlbase->addChild("url");
-        $row->addChild("loc",$site_url);
-        $row->addChild("lastmod",date("c"));
-        $row->addChild("changefreq","monthly");
-        $row->addChild("priority","1");
-
-        $row  = $xmlbase->addChild("url");
-        $row->addChild("loc",route('page_about'));
-        $row->addChild("lastmod",$defaultdate->format("Y-m-d\TH:i:sP"));
-        $row->addChild("changefreq","monthly");
-        $row->addChild("priority","1");*/
+            'portfolio.nike', 'portfolio.noiseland', 'portfolio.tocha', 'about', 'new', 'privacy.polity'];
 
         foreach ($routes as $route) {
             $this->addUrl($route, $defaultdate, $xmlbase);
