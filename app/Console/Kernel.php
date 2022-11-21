db59d38031cd061ca5a47afc30f4456344fca3ba
Gonreg <109808538+Gonreg@users.noreply.github.com>
2022-11-21 14:57:42 +0300

    siteMap

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('generate:sitemap')->everyMinute();
+        $schedule->command('generate:sitemap')->daily();
     }
 
     /**
