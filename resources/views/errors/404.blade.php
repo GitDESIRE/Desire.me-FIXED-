7f731686df9118a86e727437dcee1b9ebf929b3a
Maden51 <madenon123@gmail.com>
2022-11-21 20:40:51 +0300

    поменял href на click

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -7,7 +7,7 @@
             <div class="content-404">
                 <div class="content-text_404">
                     <p>ошибки бывают - у меня же лапки</p>
-                    <a href='#' class="btn-404">Вернуться назад</a>
+                    <a onclick="history.back()" class="btn-404">Вернуться назад</a>
                 </div>
                 <img src="{{asset('assets/svg/🦆 illustration _cat kitten animal sad pink_.svg')}}" alt="котик"/>
             </div>
